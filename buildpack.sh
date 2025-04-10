c27c4b034ce967a5def388b73bc761744a2999b1
siyu <chensiyi@agora.io>
2025-04-10 18:20:52 +0800

    add docker-compose deploy

--- buildpack.sh
+++ buildpack.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex;
-export VERSION=1.0.0
+export VERSION=1.0.1
+rm -rf deploy/*.tar
 sudo docker build . -t registry.netless.link/app/forge-persistence-private:$VERSION
-docker save registry.netless.link/app/forge-persistence-private:$VERSION -o deploy/forge-persistence-private-$VERSION.tar
+sudo docker pull nginx:latest
+sudo docker save nginx:latest -o deploy/nginx.tar
+sudo docker save registry.netless.link/app/forge-persistence-private:$VERSION -o deploy/forge-persistence-private-$VERSION.tar
 tar -czvf forge-persistence-private-$VERSION-install.tar deploy --transform s/deploy/forge-persistence/
+
