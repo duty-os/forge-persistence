1c5d10768f98585389970c10944920ff0562da1f
siyu <chensiyi@agora.io>
2026-02-04 18:31:56 +0800

    add ci deploy

--- buildpack.sh
+++ buildpack.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ex;
-export VERSION=1.0.2
+VERSION=$(node -p -e "require('./package.json').version")
+echo $VERSION
+
 rm -rf deploy/*.tar
+sed  "s/forge-persistence-private:latest/forge-persistence-private:$VERSION/" deploy/docker-compose.yaml.example > deploy/docker-compose.yaml
+cat deploy/docker-compose.yaml
 sudo docker build . -t registry.netless.link/app/forge-persistence-private:$VERSION
 sudo docker pull nginx:latest
 sudo docker save nginx:latest -o deploy/nginx.tar
