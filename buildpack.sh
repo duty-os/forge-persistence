3bd544db3b7b47d5b971b318cc4266536014142d
siyu <chensiyi@agora.io>
2026-02-04 18:36:21 +0800

    remove sudo for ci build

--- buildpack.sh
+++ buildpack.sh
@@ -6,9 +6,9 @@ echo $VERSION
 rm -rf deploy/*.tar
 sed  "s/forge-persistence-private:latest/forge-persistence-private:$VERSION/" deploy/docker-compose.yaml.example > deploy/docker-compose.yaml
 cat deploy/docker-compose.yaml
-sudo docker build . -t registry.netless.link/app/forge-persistence-private:$VERSION
-sudo docker pull nginx:latest
-sudo docker save nginx:latest -o deploy/nginx.tar
-sudo docker save registry.netless.link/app/forge-persistence-private:$VERSION -o deploy/forge-persistence-private-$VERSION.tar
+docker build . -t registry.netless.link/app/forge-persistence-private:$VERSION
+docker pull nginx:latest
+docker save nginx:latest -o deploy/nginx.tar
+docker save registry.netless.link/app/forge-persistence-private:$VERSION -o deploy/forge-persistence-private-$VERSION.tar
 tar -czvf forge-persistence-private-$VERSION-install.tar deploy --transform s/deploy/forge-persistence/
 
